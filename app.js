7f23c32a46d641a8cdd1fa97156f5ecd0e5f413e
zoeylampkin <lampkinzoey@gmail.com>
2023-11-06 11:10:31 +0100

    second commit

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
-function startGame() {
-  console.log('starting game');
-}
+// global constants
+const ROCK = 'ROCK';
+const PAPER = 'PAPER';
+const SCISSORS = 'SCISSORS';
 
-startGameBtn.addEventListener('click', startGame);
+const getPlayChoice = function () {
+  const selection = prompt(
+    `${ROCK}, ${PAPER} or ${SCISSORS}`,
+    ''
+  ).toUpperCase();
+
+  if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
+    alert('Invalid Choice! We chose Rock for you.');
+  }
+};
+
+startGameBtn.addEventListener('click', function () {
+  console.log('starting game');
+});
